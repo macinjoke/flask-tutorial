79c4624683715ed01b5bf6ec9735572a24187484
Shunji Makino <makino.shunji@stf.nifty.co.jp>
2016-10-26 02:42:09 +0000

    modify

--- hello.py
+++ hello.py
@@ -1,11 +1,10 @@
 from local_setting import host
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World!!!!"
 
 if __name__ == "__main__":
     app.run(host=host.host)
