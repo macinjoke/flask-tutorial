3713777b45a5184c217237a12fb3b104397f6b38
Shunji Makino <makino.shunji@stf.nifty.co.jp>
2016-11-01 06:00:29 +0000

    fix

--- hello.py
+++ hello.py
@@ -1,10 +1,24 @@
 from local_setting import host
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     return "Hello World!!!!"
 
+
+@app.route("/user/<username>")
+def show_user_profile(username):
+    return "User: %s" % username
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    if post_id == 0:
+        return 'post_id は 0ですね'
+    return 'Post %d' % post_id
+
 if __name__ == "__main__":
-    app.run(host=host.host)
+    app.run(host=host.host, debug=True)
